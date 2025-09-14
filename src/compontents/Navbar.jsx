5c355f4a727ede19370c224917a1ecf3deeed0e4
Aditya <anupama12sg@gmail.com>
2025-09-14 17:41:57 +0530

    navbar logo and title set in place

--- src/compontents/Navbar.jsx
+++ src/compontents/Navbar.jsx
@@ -3,7 +3,7 @@ import pokemon_logo from '/pokemon_logo.png'
 
 const Navbar = () => {
   return (
-    <div className='flex-container justify-between px-4 py-2 bg-white dark:bg-gray-900 shadow-md'>
+    <div className='flex items-center space-x-3 px-12 py-4 bg-white dark:bg-gray-900 shadow-md'>
       <div className='flex items-center space-x-3'></div>
       <img src={pokemon_logo} alt="" className='h-10 w-10'/>
       <h1 className='text-xl font-bold text-gray-900 dark:text-white'>Pokemon Cards</h1>
